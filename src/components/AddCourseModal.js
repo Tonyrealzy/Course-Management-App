4df5d534ca91400edb0e4e95a821eaf22f594798
Tonyrealzy <umehobiarinze2@gmail.com>
2024-04-21 01:54:48 +0100

    Completed form validation, just UI left

--- src/components/AddCourseModal.js
+++ src/components/AddCourseModal.js
@@ -10,15 +10,80 @@ const AddCourseModal = ({ onSave, onCancel }) => {
         enrollment_status: '',
         materials: ''
     });
-    
+
+    const [errors, setErrors] = React.useState({});
+
+    const validateName = (name) => {
+        return name.trim() !== '';
+    };
+    const validateDate = (date) => {
+        const datePattern = /^\d{4}-\d{2}-d{2}$/;
+        return datePattern.test(date);
+    };
 
     const handleChange = (event) => {
         const { name, value } = event.target;
         setNewCourse({ ...newCourse, [name]: value });
+
+        const newErrors = { ...errors };
+        switch (name) {
+            case 'name':
+                newErrors.name = validateName(value) ? '' : 'Course name is required!';
+                break;
+            case 'description':
+                newErrors.description = value.trim() !== '' ? '' : 'Course description is required!';
+                break;
+            case 'instructor':
+                newErrors.instructor = value.trim() !== '' ? '' : 'Instructor name is required!';
+                break;
+            case 'start_date':
+                newErrors.start_date = validateDate(value) ? '' : 'Invalid date format!';
+                break;
+            case 'end_date':
+                newErrors.end_date = validateDate(value) ? '' : 'Invalid date format!';
+                break;
+            case 'enrollment_status':
+            newErrors.enrollment_status = value.trim() !== '' ? '' : 'PLease fill in your course enrollment status!';
+                break;
+            case 'materials':
+                newErrors.materials = value.trim() !== '' ? '' : 'Instructional materials field cannot be left empty!';
+                break;
+            default:
+                break;
+        };
     };
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        const newErrors = {};
+
+        if (!validateName(newCourse.name)) {
+            newErrors.name = 'Course name is required!';
+        }
+        if (!validateName(newCourse.instructor)) {
+            newErrors.instructor = 'Course instructor name is required!';
+        }
+        if (!validateName(newCourse.description)) {
+            newErrors.description = 'Course description is required!';
+        }
+        if (!validateName(newCourse.start_date)) {
+            newErrors.start_date = 'Invalid date pattern!';
+        }
+        if (!validateName(newCourse.end_date)) {
+            newErrors.start_date = 'Invalid date pattern!';
+        }
+        if (!validateName(newCourse.enrollment_status)) {
+            newErrors.enrollment_status = 'Course enrollment-status field cannot be empty!';
+        }
+        if (!validateName(newCourse.materials)) {
+            newErrors.materials = 'Course materials field must be filled!';
+        }
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return;
+        }
+
         onSave(newCourse);
     };
 
@@ -28,31 +93,38 @@ const AddCourseModal = ({ onSave, onCancel }) => {
         <h2>Add Course</h2>
             <form onSubmit={handleSubmit}>
                 <label> Course Name: 
-                    <input type='text' name='name' value={ newCourse.name } onChange={handleChange}/>
+                    <input type='text' name='name' value={ newCourse.name } onChange={handleChange} />
+                    {errors.name && <p className='error'>{errors.name}</p>}
                 </label>
                 <br></br>
                 <label>Description: 
                     <input type='text' name='description' value={ newCourse.description } onChange={handleChange}/>
+                    {errors.description && <p className="error">{errors.description}</p>}
                 </label>
                 <br></br>
                 <label>Instructor: 
                     <input type='text' name='instructor' value={ newCourse.instructor } onChange={handleChange}/>
+                    {errors.instructor && <p className="error">{errors.instructor}</p>}
                 </label>
                 <br></br>
                 <label>Start Date: 
                 <input type='text' name='start_date' value={ newCourse.start_date } onChange={handleChange}/>
+                {errors.start_date && <p className="error">{errors.start_date}</p>}
                 </label>
                 <br></br>
                 <label>End Date: 
                 <input type='text' name='end_date' value={ newCourse.end_date } onChange={handleChange}/>
+                {errors.end_date && <p className='error'>{errors.end_date}</p>}
                 </label>
                 <br></br>
                 <label>Enrollment Status: 
                 <input type='text' name='enrollment_status' value={ newCourse.enrollment_status } onChange={handleChange}/>
+                {errors.enrollment_status && <p className="error">{errors.enrollment_status}</p>}
                 </label>
                 <br></br>
                 <label>Materials: 
                 <input type='text' name='materials' value={ newCourse.materials } onChange={handleChange}/>
+                {errors.materials && <p className="error">{errors.materials}</p>}
                 </label>
                 <br></br>
                 <button type='submit'>SAVE</button>
--- src/components/EditCourseModal.js
+++ src/components/EditCourseModal.js
@@ -9,8 +9,90 @@ const EditCourseModal = ({ courseData, onSave, onCancel }) => {
     const [enrollment_status, setEnrollmentStatus] = React.useState(courseData.name);
     const [materials, setMaterials] = React.useState(courseData.name);
 
+    const [errors, setErrors] = React.useState({});
+
+    const validateName = (name) => {
+        return name.trim() !== '';
+    };
+    const validateDate = (date) => {
+        const datePattern = /^\d{4}-\d{2}-d{2}$/;
+        return datePattern.test(date);
+    };
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+        const newErrors = { ...errors };
+        newErrors.name = validateName(event.target.value) ? '' : 'Course name is required';
+        setErrors(newErrors);
+      };
+      const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+        const newErrors = { ...errors };
+        newErrors.description = validateName(event.target.value) ? '' : 'Course description is required';
+        setErrors(newErrors);
+      };
+      const handleInstructorChange = (event) => {
+        setInstructor(event.target.value);
+        const newErrors = { ...errors };
+        newErrors.instructor = validateName(event.target.value) ? '' : 'Course instructor name is required';
+        setErrors(newErrors);
+      };
+      const handleEnrollmentStatusChange = (event) => {
+        setEnrollmentStatus(event.target.value);
+        const newErrors = { ...errors };
+        newErrors.enrollment_status = validateName(event.target.value) ? '' : 'Course enrollment-status is required';
+        setErrors(newErrors);
+      };
+      const handleMaterialsChange = (event) => {
+        setEnrollmentStatus(event.target.value);
+        const newErrors = { ...errors };
+        newErrors.materials = validateName(event.target.value) ? '' : 'Course materials field is required';
+        setErrors(newErrors);
+      };
+      const handleStartDateChange = (event) => {
+        setStartDate(event.target.value);
+        const newErrors = { ...errors };
+        newErrors.start_date = validateDate(event.target.value) ? '' : 'Invalid date format';
+        setErrors(newErrors);
+      };
+      const handleEndDateChange = (event) => {
+        setStartDate(event.target.value);
+        const newErrors = { ...errors };
+        newErrors.end_date = validateDate(event.target.value) ? '' : 'Invalid date format';
+        setErrors(newErrors);
+      };
+
     const handleSubmit = async (event) => {
         event.preventDefault();
+        const newErrors = {};
+
+        if (!validateName(name)) {
+            newErrors.name = 'Course name is required!';
+        }
+        if (!validateName(instructor)) {
+            newErrors.instructor = 'Course instructor name is required!';
+        }
+        if (!validateName(description)) {
+            newErrors.description = 'Course description is required!';
+        }
+        if (!validateName(start_date)) {
+            newErrors.start_date = 'Invalid date pattern!';
+        }
+        if (!validateName(end_date)) {
+            newErrors.start_date = 'Invalid date pattern!';
+        }
+        if (!validateName(enrollment_status)) {
+            newErrors.enrollment_status = 'Course enrollment-status field cannot be empty!';
+        }
+        if (!validateName(materials)) {
+            newErrors.materials = 'Course materials field must be filled!';
+        }
+
+        if (Object.keys(newErrors).length > 0) {
+            setErrors(newErrors);
+            return;
+        }
+
         const newCourseData = {
             name, description, instructor, start_date,
             end_date, enrollment_status, materials
@@ -34,31 +116,38 @@ const EditCourseModal = ({ courseData, onSave, onCancel }) => {
             <h2>Edit Course Details</h2>
             <form onSubmit={handleSubmit}>
                 <label> Course Name: 
-                    <input type='text' name='name' value={ name } onChange={(e) => setName(e.target.value)}/>
+                    <input type='text' name='name' value={ name } onChange={handleNameChange}/>
+                    {errors.name && <p className="error">{errors.name}</p>}
                 </label>
                 <br></br>
                 <label>Description: 
-                    <input type='text' name='description' value={ description } onChange={(e) => setDescription(e.target.value)}/>
+                    <input type='text' name='description' value={ description } onChange={handleDescriptionChange}/>
+                    {errors.description && <p className="error">{errors.description}</p>}
                 </label>
                 <br></br>
                 <label>Instructor: 
-                    <input type='text' name='instructor' value={ instructor } onChange={(e) => setInstructor(e.target.value)}/>
+                    <input type='text' name='instructor' value={ instructor } onChange={handleInstructorChange}/>
+                    {errors.instructor && <p className="error">{errors.instructor}</p>}
                 </label>
                 <br></br>
                 <label>Start Date: 
-                <input type='text' name='start_date' value={ start_date } onChange={(e) => setStartDate(e.target.value)}/>
+                <input type='text' name='start_date' value={ start_date } onChange={handleStartDateChange}/>
+                {errors.start_date && <p className="error">{errors.start_date}</p>}
                 </label>
                 <br></br>
                 <label>End Date: 
-                <input type='text' name='end_date' value={ end_date } onChange={(e) => setEndDate(e.target.value)}/>
+                <input type='text' name='end_date' value={ end_date } onChange={handleEndDateChange}/>
+                {errors.end_date && <p className="error">{errors.end_date}</p>}
                 </label>
                 <br></br>
                 <label>Enrollment Status: 
-                <input type='text' name='enrollment_status' value={ enrollment_status } onChange={(e) => setEnrollmentStatus(e.target.value)}/>
+                <input type='text' name='enrollment_status' value={ enrollment_status } onChange={handleEnrollmentStatusChange}/>
+                {errors.enrollment_status && <p className="error">{errors.enrollment_status}</p>}
                 </label>
                 <br></br>
                 <label>Materials: 
-                <input type='text' name='materials' value={ materials } onChange={(e) => setMaterials(e.target.value)}/>
+                <input type='text' name='materials' value={ materials } onChange={handleMaterialsChange}/>
+                {errors.materials && <p className="error">{errors.materials}</p>}
                 </label>
                 <br></br>
                 <button type='submit'>SAVE</button>
