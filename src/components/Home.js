c9de4a8974037eefdb401e18e43cf228ab1791c1
Tonyrealzy <umehobiarinze2@gmail.com>
2024-05-08 10:36:02 +0100

    Made a few changes here and there

--- src/components/Home.js
+++ src/components/Home.js
@@ -15,6 +15,8 @@ const Home = () => {
         <img className='Home1' src='https://img.freepik.com/premium-photo/photo-young-indian-woman-her-mid-20s-college-student-holding-book-her-chest_878783-7283.jpg?w=740' alt='Home1.jpg'/>
         <img className='Home3' src='https://img.freepik.com/premium-psd/hispanic-student-man-from-uk-posing-happily-with-passport-giving-thumbs-up_410516-124284.jpg?w=740' alt='Home2.jpg'/>
       </section>
+
+      <br></br>
       <ListCourses />
     </div>
   )
